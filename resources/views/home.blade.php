f2d30bc19265bbdb75843568ac6378f8d788be17
alijonovaward <alijonovaward@gmail.com>
2026-03-16 10:13:48 +0500

    saytdan txt qabul qilib bazaga saqlaydi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,2 +1,12 @@
 <h1>Hello Laravel</h1>
 <p>Bu mening birinchi sahifam</p>
+
+
+<h1>Message yuborish</h1>
+
+<form action="/message" method="POST">
+    @csrf
+    <input type="text" name="text" placeholder="Text yozing">
+
+    <button type="submit">Send</button>
+</form>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MessageController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +10,5 @@ Route::get('/', function () {
 Route::get('/home', function () {
     return view('home');
 });
+
+Route::post('/message', [MessageController::class, 'store']);
